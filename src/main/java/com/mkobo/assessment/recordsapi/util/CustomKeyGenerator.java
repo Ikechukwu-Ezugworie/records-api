f070caf67c044e2876acb17d47bdd8b62ebee56a
Ikechukwu Ezugworie <ikechukwu.ezugworie@fcmb.com>
2025-02-24 03:28:11 +0300

    refactor: updated seperator to constant

--- src/main/java/com/mkobo/assessment/recordsapi/util/CustomKeyGenerator.java
+++ src/main/java/com/mkobo/assessment/recordsapi/util/CustomKeyGenerator.java
@@ -12,11 +12,13 @@ public class CustomKeyGenerator implements KeyGenerator {
 
     Logger logger = Logger.getLogger(CustomKeyGenerator.class.getName());
 
+    private static final String SEPARATOR = "_";
+
 
     @Override
     public Object generate(Object target, Method method, Object... params) {
-        return target.getClass().getSimpleName() + "_"
-                + method.getName() + "_"
-                + StringUtils.arrayToDelimitedString(params, "_");
+        return target.getClass().getSimpleName() + SEPARATOR
+                + method.getName() + SEPARATOR
+                + StringUtils.arrayToDelimitedString(params, SEPARATOR);
     }
 }
